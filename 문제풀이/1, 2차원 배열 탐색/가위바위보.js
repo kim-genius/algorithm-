2c9e975f65af3a280e7371dca0a9473d03f74d27
kim-genius <dudwodla123@gmail.com>
2022-07-11 23:48:48 +0900

    1, 2차원 배열 탐색 완료

---
+++ 문제풀이/1, 2차원 배열 탐색/가위바위보.js
@@ -0,0 +1,19 @@
+function solution(a, b) {
+    let answer = []
+
+    for (let i = 0; i < a.length; i++) {
+
+        if (a[i] === b[i]) answer.push('D')
+        else if (a[i] === 1 && b[i] === 3) answer.push('A')
+        else if (a[i] === 2 && b[i] === 1) answer.push('A')
+        else if (a[i] === 3 && b[i] === 2) answer.push('A')
+        else answer.push('B')
+
+    }
+
+    return answer
+}
+
+const arr1 = [2, 3, 3, 1, 3]
+const arr2 = [1, 1, 2, 2, 3]
+console.log(solution(arr1, arr2).join(' '));
---
+++ 문제풀이/1, 2차원 배열 탐색/보이는 학생.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = 1
+    let max = n[0]
+
+    for (let i = 1; i < n.length; i++) {
+        if (max < n[i]) {
+            answer++;
+            max = n[i]
+        }
+    }
+
+    return answer
+}
+
+const arr = [130, 135, 148, 140, 145, 150, 150, 153]
+console.log(solution(arr));
