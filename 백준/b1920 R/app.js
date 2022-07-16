a2d169a76db07c5460c9b351a02be782cb7dbf62
kim-genius <dudwodla123@gmail.com>
2022-07-16 22:57:25 +0900

    Solve 0 / Retry 1 / Fail 1 [8979]

--- 백준/b1920 R/app.js
+++ 백준/b1920 R/app.js
@@ -4,11 +4,14 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
 let sH = new Map();
+const arr = [];
 
 for (let x of input[1].split(" ")) {
   if (!sH.has(x)) sH.set(x, true);
 }
 
 for (let x of input[3].split(" ")) {
-  sH.has(x) ? console.log(1) : console.log(0);
+  sH.has(x) ? arr.push(1) : arr.push(0);
 }
+
+console.log(arr.join("\n"));
--- 백준/b8979 F/app.js
+++ 백준/b8979 F/app.js
@@ -1,47 +1,28 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\r\n");
+console.log(input);
 
-const N = Number(input[0].split(" ")[0]);
-const K = Number(input[0].split(" ")[1]);
+// const N = Number(input[0].split(" ")[0]);
+// const K = Number(input[0].split(" ")[1]);
 
-input.shift();
+// input.shift();
 
-const rank = [...Array(N)].fill(1);
+// const arr = [];
+// const rank = [...Array(N)].fill(1);
 
-for (let i = 0; i < N; i++) {
-  for (let j = i + 1; j < N; j++) {
-    const pi = input[i].split(" ");
-    const pj = input[j].split(" ");
+// input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
 
-    if (Number(pi[1]) > Number(pj[1])) {
-      rank[j]++;
-    } else if (Number(pi[1]) < Number(pj[1])) {
-      rank[i]++;
-    }
+// for (let i = 0; i < N; i++) {
+//   const [a, ...b] = input[i].split(" ");
+//   arr.push(Number(b.join("")));
+// }
 
-    if (Number(pi[1]) === Number(pj[1]) && Number(pi[2]) > Number(pj[2])) {
-      rank[j]++;
-    } else if (
-      Number(pi[1]) === Number(pj[1]) &&
-      Number(pi[2]) < Number(pj[2])
-    ) {
-      rank[i]++;
-    }
+// for (let i = 0; i < N; i++) {
+//   for (let j = 0; j < N; j++) {
+//     if (arr[i] > arr[j]) rank[j]++;
+//   }
+// }
+// console.log(rank);
 
-    if (
-      Number(pi[1]) === Number(pj[1]) &&
-      Number(pi[2]) === Number(pj[2]) &&
-      Number(pi[3]) > Number(pj[3])
-    ) {
-      rank[j]++;
-    } else if (
-      Number(pi[1]) === Number(pj[1]) &&
-      Number(pi[2]) === Number(pj[2]) &&
-      Number(pi[3]) < Number(pj[3])
-    ) {
-      rank[i]++;
-    }
-  }
-}
-console.log(rank[K - 1]);
+// console.log(rank[K - 1]);
