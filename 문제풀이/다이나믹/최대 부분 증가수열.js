3be86bbb9e868919b559fe1a14db6757cfb30da9
kim-genius <dudwodla123@gmail.com>
2022-07-21 23:34:07 +0900

    다이나믹

---
+++ 문제풀이/다이나믹/최대 부분 증가수열.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+  let answer = 0;
+  let dy = Array.from({ length: arr.length }, () => 0);
+  dy[0] = 1;
+
+  return answer;
+}
+
+let arr = [5, 3, 7, 8, 6, 2, 9, 4];
+console.log(solution(arr));
