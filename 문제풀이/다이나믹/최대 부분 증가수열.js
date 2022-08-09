2eb47a1a06c9bc40f8e7b740e30cb9d2fca8bef4
kim-genius <dudwodla123@gmail.com>
2022-08-09 21:12:51 +0900

    다이나믹 완료

--- 문제풀이/다이나믹/최대 부분 증가수열.js
+++ 문제풀이/다이나믹/최대 부분 증가수열.js
@@ -3,6 +3,17 @@ function solution(arr) {
   let dy = Array.from({ length: arr.length }, () => 0);
   dy[0] = 1;
 
+  for (let i = 1; i < arr.length; i++) {
+    let max = 0;
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j] < arr[i] && dy[j] > max) {
+        max = dy[j];
+      }
+    }
+    dy[i] = max + 1;
+    answer = Math.max(answer, dy[i]);
+  }
+
   return answer;
 }
 
